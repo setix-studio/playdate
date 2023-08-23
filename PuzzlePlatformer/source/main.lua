7a76f8d3dd6976883bf78ba8a68d0ff283453469
setix.studio <setix.studio@gmail.com>
2023-08-23 09:46:05 -0500

    update chapter 1

--- PuzzlePlatformer/source/main.lua
+++ PuzzlePlatformer/source/main.lua
@@ -1,28 +1,22 @@
-import "dvd" -- DEMO
-local dvd = dvd(1, -1) -- DEMO
+--CoreLibs
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
+--Libraries
+import "scripts/libraries/AnimatedSprite"
+import "scripts/libraries/LDtk"
+
+import "scripts/gameScene"
+
+GameScene()
+
+local pd <const> = playdate
 local gfx <const> = playdate.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
 
-local function loadGame()
-	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
-	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
-	gfx.setFont(font) -- DEMO
-end
 
-local function updateGame()
-	dvd:update() -- DEMO
-end
-
-local function drawGame()
-	gfx.clear() -- Clears the screen
-	dvd:draw() -- DEMO
-end
-
-loadGame()
-
-function playdate.update()
-	updateGame()
-	drawGame()
-	playdate.drawFPS(0,0) -- FPS widget
+function pd.update()
+	gfx.sprite.update()
+	pd.timer.updateTimers()
 end
