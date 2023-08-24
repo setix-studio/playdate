ced5069b9a0390c429327a7fcf12b6b9b337989d
setix.studio <setix.studio@gmail.com>
2023-08-24 13:23:55 -0500

    final

--- PuzzlePlatformer/source/scripts/GameScene.lua
+++ PuzzlePlatformer/source/scripts/GameScene.lua
@@ -1,44 +1,95 @@
+local pd <const> = playdate
 local gfx <const> = playdate.graphics
+
 local ldtk <const> = LDtk
 
 TAGS = {
-	Player = 2
+    Pickup = 1,
+    Player = 2,
+    Hazard = 3
 }
 
-Z_INDEXES= {
-	Player = 100
+Z_INDEXES = {
+    Hazard = 20,
+    Pickup = 50,
+    Player = 100
 }
 
-ldtk.load('levels/world.ldtk', false)
+local usePrecomputedLevels = not pd.isSimulator
+
+ldtk.load("levels/world.ldtk", usePrecomputedLevels)
+
+if pd.isSimulator then
+    ldtk.export_to_lua_files()
+end
 
 class('GameScene').extends()
 
 function GameScene:init()
-	self:goToLevel('Level_0')
-	self.spawnX = 12 * 16
-	self.spawnY = 5 * 16
+    self:goToLevel("Level_0")
+    self.spawnX = 3 * 16
+    self.spawnY = 11 * 16
 
-	self.player = Player(self.spawnX, self.spawnY)
+    self.player = Player(self.spawnX, self.spawnY, self)
 end
 
-function GameScene:goToLevel(level_name)
-	gfx.sprite.removeAll()
+function GameScene:resetPlayer()
+    self.player:moveTo(self.spawnX, self.spawnY)
+end
 
-	for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
-		if layer.tiles then
-			local tilemap = ldtk.create_tilemap(level_name, layer_name)
+function GameScene:enterRoom(direction)
+    local level = ldtk.get_neighbours(self.levelName, direction)[1]
+    self:goToLevel(level)
+    self.player:add()
+    local spawnX, spawnY
+    if direction == "north" then
+        spawnX, spawnY = self.player.x, 240
+    elseif direction == "south" then
+        spawnX, spawnY = self.player.x, 0
+    elseif direction == "east" then
+        spawnX, spawnY = 0, self.player.y
+    elseif direction == "west" then
+        spawnX, spawnY = 400, self.player.y
+    end
+    self.player:moveTo(spawnX, spawnY)
+    self.spawnX = spawnX
+    self.spawnY = spawnY
+end
 
-			local layerSprite = gfx.sprite.new()
-			layerSprite:setTilemap(tilemap)
-			layerSprite:setCenter(0, 0)
-			layerSprite:moveTo(0, 0)
-			layerSprite:setZIndex(layer.zIndex)
-			layerSprite:add()
+function GameScene:goToLevel(levelName)
+    if not levelName then return end
 
-			local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
-			if emptyTiles then
-				gfx.sprite.addWallSprites(tilemap, emptyTiles)
-			end
-		end
-	end
+    self.levelName = levelName
+
+    gfx.sprite.removeAll()
+
+    for layerName, layer in pairs(ldtk.get_layers(levelName)) do
+        if layer.tiles then
+            local tilemap = ldtk.create_tilemap(levelName, layerName)
+
+            local layerSprite = gfx.sprite.new()
+            layerSprite:setTilemap(tilemap)
+            layerSprite:moveTo(0, 0)
+            layerSprite:setCenter(0, 0)
+            layerSprite:setZIndex(layer.zIndex)
+            layerSprite:add()
+
+            local emptyTiles = ldtk.get_empty_tileIDs(levelName, "Solid", layerName)
+            if emptyTiles then
+                gfx.sprite.addWallSprites(tilemap, emptyTiles)
+            end
+        end
+    end
+
+    for _, entity in ipairs(ldtk.get_entities(levelName)) do
+        local entityX, entityY = entity.position.x, entity.position.y
+        local entityName = entity.name
+        if entityName == "Ability" then
+            Ability(entityX, entityY, entity)
+        elseif entityName == "Spike" then
+            Spike(entityX, entityY)
+        elseif entityName == "Spikeball" then
+            Spikeball(entityX, entityY, entity)
+        end
+    end
 end
--- PuzzlePlatformer/source/scripts/player.lua
+++ PuzzlePlatformer/source/scripts/player.lua
@@ -1,79 +1,109 @@
+
 local pd <const> = playdate
 local gfx <const> = playdate.graphics
 
-class ('Player').extends(AnimatedSprite)
+class('Player').extends(AnimatedSprite)
 
-function  Player:init(x, y)
-    --State Machine
-    local playerImageTable = gfx.imagetable.new('images/player-table-16-16')
+function Player:init(x, y, gameManager)
+    self.gameManager = gameManager
+
+    -- State Machine
+    local playerImageTable = gfx.imagetable.new("images/player-table-16-16")
     Player.super.init(self, playerImageTable)
 
-    self:addState('idle', 1, 1)
-    self:addState('run', 1, 3, {tickStep = 4})
-    self:addState('jump', 4, 4)
-    self:addState('dash', 4, 4)
-
+    self:addState("idle", 1, 1)
+    self:addState("run", 1, 3, {tickStep = 4})
+    self:addState("jump", 4, 4)
+    self:addState("dash", 4, 4)
     self:playAnimation()
 
-    --Sprite Properties
+    -- Sprite properties
     self:moveTo(x, y)
     self:setZIndex(Z_INDEXES.Player)
-    self:setTag(TAGS.Player)
     self:setCollideRect(3, 3, 10, 13)
+    self:setTag(TAGS.Player)
 
-    --Physics Properties
+    -- Physics properties
     self.xVelocity = 0
-    self. yVelocity = 0
+    self.yVelocity = 0
     self.gravity = 1.0
-    self.maxSpeed = 2.0
+    self.maxSpeed = 2
     self.jumpVelocity = -6
     self.drag = 0.1
     self.minimumAirSpeed = 0.5
 
-    --Abilities
-    self.doubleJumpAbility = true
-    self.dashAbility = true
+    self.jumpBufferAmount = 5
+    self.jumpBuffer = 0
 
-    --Double Jump
+    -- Abilities
+    self.doubleJumpAbility = false
+    self.dashAbility = false
+
+    -- Double Jump
     self.doubleJumpAvailable = true
 
-    --Dash
+    -- Dash
     self.dashAvailable = true
     self.dashSpeed = 8
     self.dashMinimumSpeed = 3
     self.dashDrag = 0.8
 
-    --Player State
+    -- Player State
     self.touchingGround = false
     self.touchingCeiling = false
     self.touchingWall = false
+    self.dead = false
 end
- 
-function Player:collisionResponse()
+
+function Player:collisionResponse(other)
+    local tag = other:getTag()
+    if tag == TAGS.Pickup or tag == TAGS.Hazard then
+        return gfx.sprite.kCollisionTypeOverlap
+    end
     return gfx.sprite.kCollisionTypeSlide
 end
 
 function Player:update()
+    if self.dead then
+        return
+    end
+
     self:updateAnimation()
+
+    self:updateJumpBuffer()
     self:handleState()
-    self:handleMovementandCollisions()
+    self:handleMovementAndCollisions()
+end
+
+function Player:updateJumpBuffer()
+    self.jumpBuffer -= 1
+    if self.jumpBuffer <= 0 then
+        self.jumpBuffer = 0
+    end
+    if pd.buttonJustPressed(pd.kButtonA) then
+        self.jumpBuffer = self.jumpBufferAmount
+    end
+end
+
+function Player:playerJumped()
+    return self.jumpBuffer > 0
 end
 
 function Player:handleState()
-    if self.currentState == 'idle' then
+    if self.currentState == "idle" then
         self:applyGravity()
         self:handleGroundInput()
-    elseif self.currentState == 'run' then
+    elseif self.currentState == "run" then
         self:applyGravity()
         self:handleGroundInput()
-    elseif self.currentState == 'jump' then
+    elseif self.currentState == "jump" then
         if self.touchingGround then
             self:changeToIdleState()
         end
         self:applyGravity()
         self:applyDrag(self.drag)
         self:handleAirInput()
-    elseif self.currentState == 'dash' then
+    elseif self.currentState == "dash" then
         self:applyDrag(self.dashDrag)
         if math.abs(self.xVelocity) <= self.dashMinimumSpeed then
             self:changeToFallState()
@@ -81,25 +111,37 @@ function Player:handleState()
     end
 end
 
-function Player:handleMovementandCollisions()
+function Player:handleMovementAndCollisions()
     local _, _, collisions, length = self:moveWithCollisions(self.x + self.xVelocity, self.y + self.yVelocity)
 
     self.touchingGround = false
     self.touchingCeiling = false
     self.touchingWall = false
+    local died = false
 
-    for i=1, length do
+    for i=1,length do
         local collision = collisions[i]
-        if collision.normal.y == -1 then
-            self.touchingGround = true
-            self.doubleJumpAvailable = true
-            self.dashAvailable = true
-        elseif collision.normal.y == 1 then
-            self.touchingCeiling = true
+        local collisionType = collision.type
+        local collisionObject = collision.other
+        local collisionTag = collisionObject:getTag()
+        if collisionType == gfx.sprite.kCollisionTypeSlide then
+            if collision.normal.y == -1 then
+                self.touchingGround = true
+                self.doubleJumpAvailable = true
+                self.dashAvailable = true
+            elseif collision.normal.y == 1 then
+                self.touchingCeiling = true
+            end
+
+            if collision.normal.x ~= 0 then
+                self.touchingWall = true
+            end
         end
 
-        if collision.normal.x ~= 0 then
-            self.touchingWall = true
+        if collisionTag == TAGS.Hazard then
+            died = true
+        elseif collisionTag == TAGS.Pickup then
+            collisionObject:pickUp(self)
         end
     end
 
@@ -109,26 +151,50 @@ function Player:handleMovementandCollisions()
         self.globalFlip = 0
     end
 
+    if self.x < 0 then
+		self.gameManager:enterRoom("west")
+    elseif self.x > 400  then
+        self.gameManager:enterRoom("east")
+    elseif self.y < 0 then
+        self.gameManager:enterRoom("north")
+    elseif self.y > 240 then
+        self.gameManager:enterRoom("south")
+	end
+
+    if died then
+        self:die()
+    end
 end
 
+function Player:die()
+    self.xVelocity = 0
+    self.yVelocity = 0
+    self.dead = true
+    self:setCollisionsEnabled(false)
+    pd.timer.performAfterDelay(200, function()
+        self:setCollisionsEnabled(true)
+        self.gameManager:resetPlayer()
+        self.dead = false
+    end)
+end
 
---Input helper
+-- Input Helper Functions
 function Player:handleGroundInput()
-    if pd.buttonJustPressed(pd.kButtonA) then
+    if self:playerJumped() then
         self:changeToJumpState()
     elseif pd.buttonJustPressed(pd.kButtonB) and self.dashAvailable and self.dashAbility then
         self:changeToDashState()
     elseif pd.buttonIsPressed(pd.kButtonLeft) then
-        self:changeToRunState('left')
+        self:changeToRunState("left")
     elseif pd.buttonIsPressed(pd.kButtonRight) then
-        self:changeToRunState('right')
+        self:changeToRunState("right")
     else
         self:changeToIdleState()
     end
 end
 
 function Player:handleAirInput()
-    if pd.buttonJustPressed(pd.kButtonA) and self.doubleJumpAvailable and self.doubleJumpAbility then
+    if self:playerJumped() and self.doubleJumpAvailable and self.doubleJumpAbility then
         self.doubleJumpAvailable = false
         self:changeToJumpState()
     elseif pd.buttonJustPressed(pd.kButtonB) and self.dashAvailable and self.dashAbility then
@@ -136,43 +202,42 @@ function Player:handleAirInput()
     elseif pd.buttonIsPressed(pd.kButtonLeft) then
         self.xVelocity = -self.maxSpeed
     elseif pd.buttonIsPressed(pd.kButtonRight) then
-
+        self.xVelocity = self.maxSpeed
     end
-
 end
 
-
---State transitions
+-- State transitions
 function Player:changeToIdleState()
     self.xVelocity = 0
-    self:changeState('idle')
+    self:changeState("idle")
 end
 
 function Player:changeToRunState(direction)
-    if direction == 'left' then
+    if direction == "left" then
         self.xVelocity = -self.maxSpeed
         self.globalFlip = 1
-    elseif direction == 'right' then
+    elseif direction == "right" then
         self.xVelocity = self.maxSpeed
         self.globalFlip = 0
     end
-    self:changeState('run')
+    self:changeState("run")
 end
 
 function Player:changeToJumpState()
     self.yVelocity = self.jumpVelocity
-    self:changeState('jump')
+    self.jumpBuffer = 0
+    self:changeState("jump")
 end
 
 function Player:changeToFallState()
-    self:changeState('jump')
+    self:changeState("jump")
 end
 
 function Player:changeToDashState()
     self.dashAvailable = false
     self.yVelocity = 0
     if pd.buttonIsPressed(pd.kButtonLeft) then
-         self.xVelocity = -self.dashSpeed
+        self.xVelocity = -self.dashSpeed
     elseif pd.buttonIsPressed(pd.kButtonRight) then
         self.xVelocity = self.dashSpeed
     else
@@ -182,10 +247,10 @@ function Player:changeToDashState()
             self.xVelocity = self.dashSpeed
         end
     end
-    self:changeState('dash')
+    self:changeState("dash")
 end
 
--- Physics Helper
+-- Physics Helper Functions
 function Player:applyGravity()
     self.yVelocity += self.gravity
     if self.touchingGround or self.touchingCeiling then
@@ -203,5 +268,4 @@ function Player:applyDrag(amount)
     if math.abs(self.xVelocity) < self.minimumAirSpeed or self.touchingWall then
         self.xVelocity = 0
     end
-    
 end
